f7b38586dade1912a658a2534f509f2407506b92
Ryna2890 <irina.glonyagina@gmail.com>
2022-08-01 13:32:47 +0300

    TD-10: add checkbox logics

--- public/common/custom_checkbox/index.js
+++ public/common/custom_checkbox/index.js
@@ -1,12 +1,12 @@
-function addCheckboxListener(checkbox){
-    checkbox.addEventListener("change", (event) => {
-        let isChecked = event.target.checked
-        const checkedEvent = new CustomEvent("checkboxClick", {
-            detail: {
-                condition: isChecked,
-            },
-            bubbles: true,
-        })
-        checkbox.dispatchEvent(checkedEvent)
-    })
+function addCheckboxListener(checkbox) {
+  checkbox.addEventListener('change', (event) => {
+    let isChecked = event.target.checked;
+    const checkedEvent = new CustomEvent('checkboxClick', {
+      detail: {
+        condition: isChecked,
+      },
+      bubbles: true,
+    });
+    checkbox.dispatchEvent(checkedEvent);
+  });
 }
