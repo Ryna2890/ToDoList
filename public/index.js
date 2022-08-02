eeec7b3316dfb099fccacd24821ce3b9aaadd8e4
Larin Edgar <larin.edgar.ruslanovich@gmail.com>
2022-08-02 11:09:18 +0300

    TD-11: Style change for button Add and skrollbar

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 const root = document.querySelector('.layout');
+const taskListDone = document.querySelector('.layout__list_done_task');
 const taskListDo = document.querySelector('.layout__list_do_task');
 
 root.addEventListener('passText', (event) => {
@@ -12,10 +13,22 @@ function addTaskToList(taskItem) {
   taskListDo.appendChild(taskItem);
 }
 
-taskListDo.addEventListener('deleteEvent', (event) => {
+root.addEventListener('deleteEvent', (event) => {
   event.target.parentNode.remove();
 });
 
 root.addEventListener('checkboxClick', (event) => {
-  console.log(event.detail);
+  let checkboxCondition = event.detail.condition;
+  let checkbox = event.target;
+  doneByReplace(checkboxCondition, checkbox);
 });
+
+function doneByReplace(condition, target) {
+  if (condition) {
+    taskListDone.appendChild(target.parentNode);
+    target.parentNode.style.backgroundColor = 'rgba(217, 217, 217, 0.24)';
+  } else {
+    taskListDo.appendChild(target.parentNode);
+    target.parentNode.style.backgroundColor = '#A5A6F63D';
+  }
+}
